59c26a9a26c6b0ddab1139d065d5e9694812ff0c
SuperMonstor <senthilsudarshan@gmail.com>
2023-02-05 23:33:45 +0530

    completed welcome screen

---
+++ lib/common/content.dart
@@ -0,0 +1,6 @@
+class Content {
+  static const welcomeHeader = 'Welcome to Whatsapp';
+  static const welcomeText =
+      'Read our Privacy Policy. Tap "Agree and Continue" to accept the Terms of Service.';
+  static const welcomeButton = 'AGREE AND CONTINUE';
+}
